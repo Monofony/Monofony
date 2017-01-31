92b2b56436cc8f1438f10a4a621a879cc8bfcc50
corentin <corentin@mobizel.com>
2017-01-31 11:01:44 +0100

    [A] add grid for customer

---
+++ src/AppBundle/Form/Type/Customer/CustomerType.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form\Type\Customer;
+
+use Sylius\Bundle\CustomerBundle\Form\Type\CustomerType as BaseCustomerType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Michał Marcinkowski <michal.marcinkowski@lakion.com>
+ */
+class CustomerType
+{
+    /**
+     * @var EventSubscriberInterface
+     */
+    private $addUserFormSubscriber;
+
+    /**
+     * @param string $dataClass
+     * @param string[] $validationGroups
+     * @param EventSubscriberInterface $addUserFormSubscriber
+     */
+    public function __construct(
+        $dataClass,
+        array $validationGroups = [],
+        EventSubscriberInterface $addUserFormSubscriber
+    ) {
+      //  parent::__construct($dataClass, $validationGroups);
+        $this->addUserFormSubscriber = $addUserFormSubscriber;
+    }
+
+    public function getParent()
+    {
+        return BaseCustomerType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+//        parent::buildForm($builder, $options);
+
+        $builder
+            ->addEventSubscriber($this->addUserFormSubscriber);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'cascade_validation' => true,
+        ]);
+    }
+}
