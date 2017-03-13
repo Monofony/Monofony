73aea1e63aadf264bf716b6db38b5efd4e7f13eb
Loic Fremont <loic@mobizel.com>
2017-03-13 10:24:01 +0100

    [a] add creating address api test case

--- tests/Controller/AddressApiTest.php
+++ tests/Controller/AddressApiTest.php
@@ -20,13 +20,29 @@ use Symfony\Component\HttpFoundation\Response;
 class AddressApiTest extends JsonApiTestCase
 {
     /**
+     * @var array
+     */
+    private static $authorizedHeaderWithContentType = [
+        'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        'CONTENT_TYPE' => 'application/json',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $authorizedHeaderWithAccept = [
+        'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        'ACCEPT' => 'application/json',
+    ];
+
+    /**
      * @test
      */
     public function it_does_not_allow_to_show_taxon_when_it_does_not_exist()
     {
         $this->loadFixturesFromFile('resources/addresses.yml');
 
-        $this->client->request('GET', '/api/addresses/-1');
+        $this->client->request('GET', '/api/addresses/-1', [], [], static::$authorizedHeaderWithAccept);
 
         $response = $this->client->getResponse();
         $this->assertResponse($response, 'error/not_found_response', Response::HTTP_NOT_FOUND);
@@ -38,10 +54,30 @@ class AddressApiTest extends JsonApiTestCase
     public function it_allows_indexing_addresses()
     {
         $this->loadFixturesFromFile('resources/addresses.yml');
-        $this->client->request('GET', '/api/addresses/');
+        $this->client->request('GET', '/api/addresses/', [], [], static::$authorizedHeaderWithAccept);
 
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'address/index_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_allows_creating_address()
+    {
+        $data =
+            <<<EOT
+                    {
+            "street": "2 rue de la Mabilais",
+            "postcode": "35000",
+            "city": "Rennes"
+        }
+EOT;
+
+        $this->client->request('POST', '/api/addresses/', [], [], static::$authorizedHeaderWithContentType, $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'address/create_response', Response::HTTP_CREATED);
     }
 }
