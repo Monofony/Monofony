ee34a6fb130fcefe4a2dbcb33a66e3b9b5f8b638
conicole <corentin@mobizel.com>
2017-03-13 17:50:35 +0100

    [A] add Oauth api connection

---
+++ src/AppBundle/Entity/OAuth/AccessToken.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of AppName.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace AppBundle\Entity\OAuth;
+
+use FOS\OAuthServerBundle\Entity\AccessToken as BaseAccessToken;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class AccessToken extends BaseAccessToken
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $client;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\AppUser")
+     */
+    protected $user;
+}
---
+++ src/AppBundle/Entity/OAuth/Client.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of AppName.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace AppBundle\Entity\OAuth;
+
+use FOS\OAuthServerBundle\Entity\Client as BaseClient;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Client extends BaseClient
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
