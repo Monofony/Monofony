39dbfa9f60ac6fffc6b7bccc4581786d89815042
kévin regnier <kevin@MacBook-Pro-de-kevin.local>
2017-04-05 17:31:58 +0200

    [I] use codeship

---
+++ codeship.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Set php version through phpenv. 5.3, 5.4 and 5.5 available
+phpenv local 7.0
+
+# Install dependencies through Composer
+composer install --prefer-dist --no-interaction
+
+# mysql
+sed -i 's/database_host.*/database_host: 127.0.0.1/' app/config/parameters.yml
+sed -i "s/database_user.*/database_user: ${MYSQL_USER}/" app/config/parameters.yml
+sed -i "s/database_password.*/database_password: ${MYSQL_PASSWORD}/" app/config/parameters.yml
+sed -i "s/dbname.*/dbname: test/" app/config/config_test.yml
+
+# php
+sed -i'' 's/^memory_limit=.*/memory_limit = -1/g' ${HOME}/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+# database creation
+php bin/console doctrine:migrations:migrate --env=test -n
+php bin/console cache:clear --no-warmup --env=test
