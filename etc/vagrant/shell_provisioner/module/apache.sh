0a4c6108619972aea497a0c45fc01a3f200f0d79
Steve Moreau <stevemoreau@users.noreply.github.com>
2017-06-29 08:55:31 +0200

    [F] Improved logging in vagrant provisioning [A] Added a check after PHP installation to be sure Xdebug is properly installed too

--- etc/vagrant/shell_provisioner/module/apache.sh
+++ etc/vagrant/shell_provisioner/module/apache.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# Apache
+plog "Instaling apache2 and its dependencies"
 apt-get install -y apache2 libapache2-mod-fcgid
 a2enmod rewrite expires headers proxy proxy_http proxy_fcgi actions fastcgi alias ssl
 
-# Activate vhost
+plog "Activating virtual host"
 a2dissite 000-default
 
 chmod -R a+rX /var/log/apache2
 sed -i 's/640/666/' /etc/logrotate.d/apache2
 
+plog "Setting up ports to listen"
 echo 'Listen 80
       Listen 443
 ' >  /etc/apache2/ports.conf
 
+plog "Adding application to the available sites"
 cat ${CONFIG_PATH}/apache/app.vhost.conf > /etc/apache2/sites-available/app.conf
 a2ensite app.conf
 
+plog "Restarting apache2"
 service apache2 restart
--- etc/vagrant/shell_provisioner/module/tools.sh
+++ etc/vagrant/shell_provisioner/module/tools.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# Tools
+plog "Installing useful tools such as zip, curl etc."
 apt-get install -y zip unzip curl git vim
+
+plog "Installing some aliases"
+cat >> /home/vagrant/.bashrc <<EOF
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias www='cd /var/www/$APP_DBNAME'
+alias db='mysql -uroot -pvagrant ${APP_DBNAME}_dev'
+EOF
