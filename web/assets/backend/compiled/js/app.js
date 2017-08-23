a279bcb6ff6f114bdff55578a7b1f38f342a7661
Loic Fremont <loic@mobizel.com>
2017-08-23 16:25:46 +0200

    add errors on tab

--- web/assets/backend/compiled/js/app.js
+++ web/assets/backend/compiled/js/app.js
@@ -2215,6 +2215,8 @@ if(n.debug("Changing setting",t,o),e.isPlainObject(t))e.extend(!0,f,t);else{if(o
 (function ($) {
     $(document).ready(function () {
         $('.sylius-autocomplete').autoComplete();
+        $('.sylius-tabular-form').addTabErrors();
+        $('.ui.accordion').addAccordionErrors();
         $('#sylius_customer_createUser').change(function () {
             $('#user-form').toggle();
         });
@@ -2224,3 +2226,50 @@ if(n.debug("Changing setting",t,o),e.isPlainObject(t))e.extend(!0,f,t);else{if(o
     });
 })(jQuery);
 
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+(function ($) {
+    'use strict';
+
+    $.fn.extend({
+        addTabErrors: function () {
+            var element = $(this);
+
+            $('.ui.segment > .ui.tab').each(function () {
+                var errors = $(this).find('.sylius-validation-error');
+
+                if(0 !== errors.length) {
+                    var tabName = $(this).attr('data-tab');
+                    var tabWithErrors = $(element).find('a.item[data-tab="' + tabName + '"]');
+
+                    var label = tabWithErrors.html();
+                    var newLabel = label + '<span class="ui small horizontal circular label" style="background-color: #DB2828">' + errors.length + '</span>';
+
+                    tabWithErrors.html(newLabel);
+                }
+            });
+        },
+        addAccordionErrors: function () {
+            var element = $(this);
+            var accordionElements = element.find('.ui.content');
+
+            $(accordionElements).each(function () {
+                var errors = $(this).find('.sylius-validation-error');
+
+                if(0 !== errors.length) {
+                    var ribWithErrors = $(this).closest('[data-locale]').find('.title');
+
+                    ribWithErrors.css('color', '#DB2828');
+                }
+            });
+        }
+    })
+})( jQuery );
