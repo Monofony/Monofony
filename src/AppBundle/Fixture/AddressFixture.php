9380ac2aad30a038b9ecd04f48bab0f0b9f9deb8
Loic Fremont <loic@mobizel.com>
2017-09-28 12:07:38 +0200

    upgrade sylius with stable release

--- src/AppBundle/Fixture/AddressFixture.php
+++ src/AppBundle/Fixture/AddressFixture.php
@@ -21,7 +21,7 @@ class AddressFixture extends AbstractResourceFixture
     /**
      * {@inheritdoc}
      */
-    public function getName()
+    public function getName(): string
     {
         return 'address';
     }
--- src/AppBundle/Fixture/AdminUserFixture.php
+++ src/AppBundle/Fixture/AdminUserFixture.php
@@ -21,7 +21,7 @@ class AdminUserFixture extends AbstractResourceFixture
     /**
      * {@inheritdoc}
      */
-    public function getName()
+    public function getName(): string
     {
         return 'admin_user';
     }
