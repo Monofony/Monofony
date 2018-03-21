18d21f80a0c97725b0184bdae32c59bc18a8bcef
= <lc.fremont@gmail.com>
2018-03-21 14:43:38 +0000

    remove mobizel mentions

--- src/AppBundle/Behat/CustomerContext.php
+++ src/AppBundle/Behat/CustomerContext.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of VPS.
  *
- * (c) Mobizel
+ * (c) Monofony
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
