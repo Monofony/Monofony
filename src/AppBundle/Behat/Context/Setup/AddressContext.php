2ab875075380a3b71ed6945488e94b8ff57ae45a
= <lc.fremont@gmail.com>
2018-03-29 09:29:38 +0100

    use new setup address context

--- src/AppBundle/Behat/Context/Setup/AddressContext.php
+++ src/AppBundle/Behat/Context/Setup/AddressContext.php
@@ -16,16 +16,33 @@ use AppBundle\Behat\DefaultContext;
 class AddressContext extends DefaultContext
 {
     /**
+     * @Given there is an address located at :city
+     */
+    public function thereIsAddressLocatedAtCity(string $city)
+    {
+        $this->createAddresses(1, ['city' => $city]);
+    }
+
+    /**
      * @Given there are :count addresses located at :city
      */
-    public function thereAreAddressesLocatedAtCity($count, $city)
+    public function thereAreAddressesLocatedAtCity(int $count, string $city)
+    {
+        $this->createAddresses((int) $count, ['city' => $city]);
+    }
+
+    /**
+     * @param int $count
+     *
+     * @param array $options
+     */
+    private function createAddresses(int $count, array $options = []): void
     {
         $manager = $this->getEntityManager();
         $factory = $this->getExampleFactory('address');
 
         for ($i=0 ; $i<$count ; $i++) {
-            $address = $factory->create(['city' => $city]);
-
+            $address = $factory->create($options);
             $manager->persist($address);
         }
 
