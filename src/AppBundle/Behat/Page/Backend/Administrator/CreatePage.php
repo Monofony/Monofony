90c8f2877dd8892a3516b7f55f092875eec22046
Loic Fremont <lc.fremont@gmail.com>
2018-06-08 00:15:58 +0200

    [Behat] adding administrator

---
+++ src/AppBundle/Behat/Page/Backend/Administrator/CreatePage.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace AppBundle\Behat\Page\Backend\Administrator;
+
+use AppBundle\Behat\Page\Backend\Crud\CreatePage as BaseCreatePage;
+use AppBundle\Behat\Page\Backend\Crud\CreatePageInterface;
+
+class CreatePage extends BaseCreatePage implements CreatePageInterface
+{
+    public function enable()
+    {
+        $this->getElement('enabled')->check();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function specifyUsername($username)
+    {
+        $this->getElement('name')->setValue($username);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function specifyEmail($email)
+    {
+        $this->getElement('email')->setValue($email);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function specifyPassword($password)
+    {
+        $this->getElement('password')->setValue($password);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function specifyLocale($localeCode)
+    {
+        $this->getElement('locale_code')->selectOption($localeCode);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefinedElements()
+    {
+        return array_merge(parent::getDefinedElements(), [
+            'email' => '#sylius_admin_user_email',
+            'enabled' => '#sylius_admin_user_enabled',
+            'locale_code' => '#sylius_admin_user_localeCode',
+            'name' => '#sylius_admin_user_username',
+            'password' => '#sylius_admin_user_plainPassword',
+        ]);
+    }
+}
