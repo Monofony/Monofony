b121ef18194a0183967d597d6d087b9c9ee63d92
Loic Fremont <lc.fremont@gmail.com>
2018-06-08 00:15:58 +0200

    [Behat] adding administrator

---
+++ src/AppBundle/Behat/Page/Backend/Administrator/UpdatePage.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace AppBundle\Behat\Page\Backend\Administrator;
+
+use AppBundle\Behat\Page\Backend\Crud\UpdatePageInterface;
+use AppBundle\Behat\Page\Backend\Crud\UpdatePage as BaseUpdatePage;
+
+class UpdatePage extends BaseUpdatePage implements UpdatePageInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function changeUsername($username)
+    {
+        $this->getElement('username')->setValue($username);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function changeEmail($email)
+    {
+        $this->getElement('email')->setValue($email);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function changePassword($password)
+    {
+        $this->getElement('password')->setValue($password);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function changeLocale($localeCode)
+    {
+        $this->getElement('locale_code')->selectOption($localeCode);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefinedElements()
+    {
+        return array_merge(parent::getDefinedElements(), [
+            'email' => '#sylius_admin_user_email',
+            'enabled' => '#sylius_admin_user_enabled',
+            'locale_code' => '#sylius_admin_user_localeCode',
+            'password' => '#sylius_admin_user_plainPassword',
+            'username' => '#sylius_admin_user_username',
+        ]);
+    }
+}
