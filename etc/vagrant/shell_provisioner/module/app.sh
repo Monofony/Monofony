e6cb44722ceaa2d894b91ffdd17d73a63141635b
Loic Fremont <lc.fremont@gmail.com>
2018-06-08 23:52:24 +0200

    use yarn

--- etc/vagrant/shell_provisioner/module/app.sh
+++ etc/vagrant/shell_provisioner/module/app.sh
@@ -6,3 +6,6 @@ composer install --optimize-autoloader
 
 php bin/console app:install --no-interaction
 php bin/console sylius:fixtures:load --no-interaction
+cd web/assets/backend
+yarn install
+yarn run gulp
