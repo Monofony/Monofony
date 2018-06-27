387ccd293d3a8df7ed536f5a1516642e36a9b9a5
loic425 <lc.fremont@gmail.com>
2018-06-27 11:30:49 +0200

    jedisjeux replacements

--- etc/bash/common.lib.sh
+++ etc/bash/common.lib.sh
@@ -119,23 +119,23 @@ get_number_of_jobs_for_parallel()
     echo "${jobs}"
 }
 
-get_sylius_path()
+get_app_name_path()
 {
     echo "$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
 }
 
-has_sylius_cache() {
-    if [[ ! -z "${SYLIUS_CACHE_DIR}" && -d "${SYLIUS_CACHE_DIR}" ]]; then
+has_app_name_cache() {
+    if [[ ! -z "${APP_NAME_CACHE_DIR}" && -d "${APP_NAME_CACHE_DIR}" ]]; then
         return 0
     else
         return 1
     fi
 }
 
-inform_about_sylius_cache() {
-    if ! has_sylius_cache; then
+inform_about_app_name_cache() {
+    if ! has_app_name_cache; then
         print_warning "Sylius cache should be used, but it is not configured correctly."
-        print_warning "Check whether you have \$SYLIUS_CACHE_DIR set and if that directory exists."
+        print_warning "Check whether you have \$APP_NAME_CACHE_DIR set and if that directory exists."
     fi
 }
 
@@ -178,9 +178,9 @@ is_package_cache_fresh() {
     local package_path="$(cast_package_argument_to_package_path "$1")"
     local cache_key="$(get_package_cache_key "$1")"
 
-    if [[ -f "${SYLIUS_CACHE_DIR}/composer-${cache_key}.lock" && -f "${SYLIUS_CACHE_DIR}/composer-${cache_key}.json.md5sum" ]]; then
+    if [[ -f "${APP_NAME_CACHE_DIR}/composer-${cache_key}.lock" && -f "${APP_NAME_CACHE_DIR}/composer-${cache_key}.json.md5sum" ]]; then
         current_hash="$(file_md5sum "${package_path}/composer.json")"
-        cached_hash="$(cat "${SYLIUS_CACHE_DIR}/composer-${cache_key}.json.md5sum")"
+        cached_hash="$(cat "${APP_NAME_CACHE_DIR}/composer-${cache_key}.json.md5sum")"
 
         if [ "${current_hash}" = "${cached_hash}" ]; then
             return 0
--- etc/travis/suites/common/after_failure.sh
+++ etc/travis/suites/common/after_failure.sh
@@ -3,7 +3,7 @@
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 
 print_header "Uploading logs" "AppName"
-run_command "vendor/lakion/mink-debug-extension/travis/tools/upload-textfiles \"${JEDISJEUX_BUILD_DIR}/*.log\""
+run_command "vendor/lakion/mink-debug-extension/travis/tools/upload-textfiles \"${APP_NAME_BUILD_DIR}/*.log\""
 
 print_header "Uploading screenshots" "AppName"
-run_command "IMGUR_API_KEY=4907fcd89e761c6b07eeb8292d5a9b2a vendor/lakion/mink-debug-extension/travis/tools/upload-screenshots \"${JEDISJEUX_BUILD_DIR}/*.png\""
+run_command "IMGUR_API_KEY=4907fcd89e761c6b07eeb8292d5a9b2a vendor/lakion/mink-debug-extension/travis/tools/upload-screenshots \"${APP_NAME_BUILD_DIR}/*.png\""
