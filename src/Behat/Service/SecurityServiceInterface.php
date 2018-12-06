55c068e53c2cf7f7daef1aede85cb818b3b00c8e
Loïc Frémont <lc.fremont@gmail.com>
2018-12-06 14:57:26 +0100

    update head comment blocks

--- src/Behat/Service/SecurityServiceInterface.php
+++ src/Behat/Service/SecurityServiceInterface.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Sylius package.
+ * This file is part of AppName.
  *
- * (c) Paweł Jędrzejewski
+ * (c) Monofony
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
