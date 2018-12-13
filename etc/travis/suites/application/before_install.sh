cee101ad767184b9d9f083a87ad65ca21ed824a8
Loïc Frémont <lc.fremont@gmail.com>
2018-12-13 15:28:31 +0100

    fix install assets on travis

--- etc/travis/suites/application/before_install.sh
+++ etc/travis/suites/application/before_install.sh
@@ -6,16 +6,5 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/application.
 print_header "Activating memcached extension" "AppName"
 run_command "echo \"extension = memcached.so\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $?
 
-print_header "Installing chromium" "AppName"
-run_command "sudo apt-get install -y chromium-browser"
-
-print_header "Installing Yarn" "AppName"
-
-# Install Node Version Manager to install newer node version
-run_command "rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout \`git describe --abbrev=0 --tags\`) && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION" || exit $?
-
-# Install Yarn globally
-run_command "sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg"
-run_command "echo \"deb http://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list"
-run_command "sudo apt-get update -qq"
-run_command "sudo apt-get install -y -qq yarn=0.21.3-1"
+print_header "Updating Composer" "AppName"
+run_command "composer self-update --preview"
