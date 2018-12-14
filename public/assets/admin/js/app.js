6a1bbbe222ee1e610025a2e5e0a978225e8df4fe
Loïc Frémont <lc.fremont@gmail.com>
2018-12-14 17:32:45 +0100

    [WIP] gulp babel

---
+++ public/assets/admin/js/app.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+
+    (function ($) {
+        $(document).ready(function () {
+            $('.sylius-autocomplete').autoComplete();
+            $('.sylius-tabular-form').addTabErrors();
+            $('.ui.accordion').addAccordionErrors();
+            $('#sylius_customer_createUser').change(function () {
+                $('#user-form').toggle();
+            });
+
+            $('.app-date-picker').datePicker();
+            $('.app-date-time-picker').dateTimePicker();
+        });
+    })(jQuery);
+
+}());
+//# sourceMappingURL=app.js.map
