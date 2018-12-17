496044bf19bd1d861ea6e85ad66248cefa263d60
Loïc Frémont <lc.fremont@gmail.com>
2018-12-17 17:54:10 +0100

    update account templates with semantic ui

---
+++ src/Menu/AccountMenuBuilder.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of AppName.
+ *
+ * (c) Monofony
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+final class AccountMenuBuilder
+{
+    public const EVENT_NAME = 'sylius.menu.app.account';
+
+    /** @var FactoryInterface */
+    private $factory;
+
+    /** @var EventDispatcherInterface */
+    private $eventDispatcher;
+
+    public function __construct(FactoryInterface $factory, EventDispatcherInterface $eventDispatcher)
+    {
+        $this->factory = $factory;
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    public function createMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setLabel('sylius.ui.my_account');
+
+        $menu
+            ->addChild('dashboard', ['route' => 'sylius_frontend_account_dashboard'])
+            ->setLabel('sylius.ui.dashboard')
+            ->setLabelAttribute('icon', 'home')
+        ;
+        $menu
+            ->addChild('personal_information', ['route' => 'sylius_frontend_account_profile_update'])
+            ->setLabel('sylius.ui.personal_information')
+            ->setLabelAttribute('icon', 'user')
+        ;
+        $menu
+            ->addChild('change_password', ['route' => 'sylius_frontend_account_change_password'])
+            ->setLabel('sylius.ui.change_password')
+            ->setLabelAttribute('icon', 'lock')
+        ;
+
+        $this->eventDispatcher->dispatch(self::EVENT_NAME, new MenuBuilderEvent($this->factory, $menu));
+
+        return $menu;
+    }
+}
