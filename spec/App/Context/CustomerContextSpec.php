fb7c99d4c2784d8cf8ff086e549ee4bf230d0f98
Loic Fremont <lc.fremont@gmail.com>
2019-02-20 22:21:32 +0100

    add some missing phpspec

---
+++ spec/App/Context/CustomerContextSpec.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of AppName.
+ *
+ * (c) Monofony
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\App\Context;
+
+use App\Entity\Customer;
+use App\Entity\AppUser;
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+final class CustomerContextSpec extends ObjectBehavior
+{
+    function let(TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker): void
+    {
+        $this->beConstructedWith($tokenStorage, $authorizationChecker);
+    }
+
+    function it_gets_customer_from_currently_logged_user(
+        TokenStorageInterface $tokenStorage,
+        AuthorizationCheckerInterface $authorizationChecker,
+        TokenInterface $token,
+        AppUser $user,
+        Customer $customer
+    ): void {
+        $tokenStorage->getToken()->willReturn($token);
+        $authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')->willReturn(true);
+        $token->getUser()->willReturn($user);
+        $user->getCustomer()->willReturn($customer);
+
+        $this->getCustomer()->shouldReturn($customer);
+    }
+
+    function it_returns_null_if_user_is_not_logged_in($tokenStorage): void
+    {
+        $tokenStorage->getToken()->willReturn(null);
+
+        $this->getCustomer()->shouldReturn(null);
+    }
+
+    function it_returns_null_if_user_is_not_an_user_instance(
+        TokenStorageInterface $tokenStorage,
+        AuthorizationCheckerInterface $authorizationChecker,
+        TokenInterface $token,
+        \stdClass $user
+    ): void {
+        $tokenStorage->getToken()->willReturn($token);
+        $authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')->willReturn(true);
+        $token->getUser()->willReturn($user);
+
+        $this->getCustomer()->shouldReturn(null);
+    }
+}
