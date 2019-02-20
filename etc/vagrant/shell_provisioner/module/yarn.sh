e7a850e1623d8937ae049b5036b8ccb8a30d6d0a
Loïc Frémont <lc.fremont@gmail.com>
2019-02-20 16:05:41 +0100

    Remove vagrant

--- etc/vagrant/shell_provisioner/module/yarn.sh
+++
@@ -1,5 +0,0 @@
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-
-apt-get update
-apt-get install -y yarn
