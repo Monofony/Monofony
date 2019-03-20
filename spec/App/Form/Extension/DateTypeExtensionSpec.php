1ad1b781f23271de897f4e15d36dbe814f209b26
Loïc Frémont <lc.fremont@gmail.com>
2019-03-20 14:48:04 +0100

    improve infection

---
+++ spec/App/Form/Extension/DateTypeExtensionSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace spec\App\Form\Extension;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DateTypeExtensionSpec extends ObjectBehavior
+{
+    function it_extends_an_abstract_type_extension()
+    {
+        $this->shouldHaveType(AbstractTypeExtension::class);
+    }
+
+    function it_extends_date_type()
+    {
+        $this::getExtendedTypes()->shouldReturn([DateType::class]);
+    }
+
+    function it_configures_options(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(Argument::type('array'))->shouldBeCalled();
+
+        $this->configureOptions($resolver);
+    }
+}
