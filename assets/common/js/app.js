9380e34becfb33b252748e81ab3f605e2a022ea6
smildev <kevin@mobizel.com>
2019-08-02 16:18:17 +0200

    import sylius-auto-complete

--- assets/common/js/app.js
+++ assets/common/js/app.js
@@ -3,3 +3,4 @@ import './shim-semantic-ui';
 
 import 'semantic-ui-css/components/accordion';
 import '../../../vendor/sylius/ui-bundle/Resources/private/js/app';
+import '../../../vendor/sylius/ui-bundle/Resources/private/js/sylius-auto-complete';
