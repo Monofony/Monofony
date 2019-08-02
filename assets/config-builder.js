f014a1f5ee15765e1a1ef69dca465add3eb233f2
smildev <kevin@mobizel.com>
2019-08-02 17:10:42 +0200

    separate frontend and backend config

---
+++ assets/config-builder.js
@@ -0,0 +1,50 @@
+var upath = require('upath');
+var path = require('path');
+var Encore = require('@symfony/webpack-encore');
+
+const vendorUiPath = '../../vendor/sylius/ui-bundle';
+
+var build = (name) => {
+    Encore
+    // the project directory where compiled assets will be stored
+        .setOutputPath(`public/assets/${name}`)
+        // the public path used by the web server to access the previous directory
+        .setPublicPath(`/assets/${name}`)
+        .cleanupOutputBeforeBuild()
+        .enableSourceMaps(!Encore.isProduction())
+        .enableVersioning(Encore.isProduction())
+        // uncomment to define the assets of the project
+        .addEntry('app', `./assets/${name}/js/app.js`)
+        // uncomment if you use Sass/SCSS files
+        .enableSassLoader((options) => {
+            options.data = '@import "~semantic-ui-css/semantic.min.css";';
+        })
+        .autoProvidejQuery()
+        .configureBabel()
+        .disableSingleRuntimeChunk()
+        .copyFiles({
+            from: `./assets/${name}/img`,
+            to: 'img/[path][name].[ext]',
+        }, {
+            from: upath.joinSafe(vendorUiPath, 'Resources/private/img'),
+            to: 'img/[path][name].[ext]',
+        })
+        .configureFilenames({
+            js: 'js/[name].[hash:8].js',
+            css: 'css/[name].[hash:8].css',
+            images: 'img/[name].[hash:8].[ext]',
+            fonts: 'font/[name].[hash:8].[ext]'
+        });
+    ;
+
+    const config = Encore.getWebpackConfig();
+    config.name = name;
+    config.resolve.alias = {
+        '~': path.resolve(__dirname, '../../')
+    };
+
+    Encore.reset();
+
+    return config;
+};
+module.exports = build;
