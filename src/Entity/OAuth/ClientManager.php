7e5e1b083f7e53632eb543008c350148d0067fa7
Loïc Frémont <loic@mobizel.com>
2019-11-28 12:31:10 +0100

    Add OAuth login

---
+++ src/Entity/OAuth/ClientManager.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of AppName.
+ *
+ * (c) Monofony
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Entity\OAuth;
+
+use FOS\OAuthServerBundle\Entity\ClientManager as BaseClientManager;
+use FOS\OAuthServerBundle\Model\ClientInterface;
+
+class ClientManager extends BaseClientManager
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function findClientByPublicId($publicId): ?ClientInterface
+    {
+        return $this->findClientBy(['randomId' => $publicId]);
+    }
+}
