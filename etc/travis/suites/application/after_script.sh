0443d2abdf0645cdd81da427653dda089384835a
Loïc Frémont <lc.fremont@gmail.com>
2020-02-21 17:17:55 +0100

    Fix import

--- etc/travis/suites/application/after_script.sh
+++ etc/travis/suites/application/after_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../../bash/common.lib.sh"
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 
 if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]]; then
     run_command "vendor/bin/monorepo-builder split -v"
