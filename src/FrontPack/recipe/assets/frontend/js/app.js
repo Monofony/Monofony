35a44cab13cb686e80ae4f7f9dfbef4f75f3ab09
Loïc Frémont <lc.fremont@gmail.com>
2020-02-21 16:17:57 +0100

    [WIP] init webpack config

---
+++ src/FrontPack/recipe/assets/frontend/js/app.js
@@ -0,0 +1,10 @@
+import "sylius/ui/sass/main";
+import '../scss/main.scss';
+
+import 'babel-polyfill';
+import './shim-semantic-ui';
+
+import 'semantic-ui-css/components/accordion';
+
+$(document).ready(function () {
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-var backendConfig = require('./assets/backend/webpack.config');
-var frontendConfig = require('./assets/frontend/webpack.config');
+const path = require('path');
+const vendorUiPath = path.resolve(__dirname, 'vendor/sylius/ui-bundle');
+const build = require('./src/CorePack/recipe/config-builder');
+
+const backendConfig = build('backend', `./src/AdminPack/recipe/assets/backend/js/app.js`, vendorUiPath);
+const frontendConfig = build('frontend', `./src/FrontPack/recipe/assets/frontend/js/app.js`, vendorUiPath);
 
 module.exports = [backendConfig, frontendConfig];
