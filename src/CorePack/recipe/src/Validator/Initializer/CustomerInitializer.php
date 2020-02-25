729273afb54023b29fe9a6a1a4be2f5f95933521
Loïc Frémont <lc.fremont@gmail.com>
2020-02-25 11:48:20 +0100

    Test coding standard

--- src/CorePack/recipe/src/Validator/Initializer/CustomerInitializer.php
+++ src/CorePack/recipe/src/Validator/Initializer/CustomerInitializer.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * This file is part of AppName.
- *
- * (c) Monofony
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 namespace App\Validator\Initializer;
 
