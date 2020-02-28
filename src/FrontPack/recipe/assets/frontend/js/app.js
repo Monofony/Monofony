3c32cf1acd18e09f4a1383eb7ff7b9df6ac9f183
regnier kevin <regnierk@gmail.com>
2020-02-28 14:33:02 +0100

    finalize main webpack

--- src/FrontPack/recipe/assets/frontend/js/app.js
+++ src/FrontPack/recipe/assets/frontend/js/app.js
@@ -1,10 +1,7 @@
-import "sylius/ui/sass/main";
 import '../scss/main.scss';
 
 import 'babel-polyfill';
 import './shim-semantic-ui';
-
-import 'semantic-ui-css/components/accordion';
 
 $(document).ready(function () {
 });
