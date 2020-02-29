2f4a77bd301d162959f1587f4ba542f3eee864b6
Loic Fremont <lc.fremont@gmail.com>
2020-02-29 15:47:32 +0100

    Configure Admin dashboard statistics

--- src/Monofony/Bundle/AdminBundle/Recipe/src/Controller/DashboardController.php
+++ src/Monofony/Bundle/AdminBundle/Recipe/src/Controller/DashboardController.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Dashboard\DashboardStatisticsProvider;
+use Monofony\Bundle\AdminBundle\Dashboard\DashboardStatisticsProviderInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Templating\EngineInterface;
 
 final class DashboardController
 {
-    /** @var DashboardStatisticsProvider */
+    /** @var DashboardStatisticsProviderInterface */
     private $statisticsProvider;
 
     /** @var EngineInterface */
     private $templating;
 
-    public function __construct(DashboardStatisticsProvider $statisticsProvider, EngineInterface $templating)
+    public function __construct(DashboardStatisticsProviderInterface $statisticsProvider, EngineInterface $templating)
     {
         $this->statisticsProvider = $statisticsProvider;
         $this->templating = $templating;
