66d88036e93589e77e2c0cba7fdffa1def85db56
Loic Fremont <lc.fremont@gmail.com>
2020-02-29 02:07:31 +0100

    Move webpack core config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const vendorUiPath = path.resolve(__dirname, 'vendor/sylius/ui-bundle');
-const build = require('./src/CorePack/recipe/config-builder');
+const build = require('./src/Monofony/Bundle/CoreBundle/Recipe/config-builder');
 
 const backendConfig = build('backend', `./src/Monofony/Bundle/AdminBundle/Recipe/assets/backend/`, vendorUiPath);
 const frontendConfig = build('frontend', `./src/FrontPack/recipe/assets/frontend/`, vendorUiPath);
