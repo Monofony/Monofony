9e91c18a34a8ee4856324d7f27aae6849bf312a0
Loic Fremont <lc.fremont@gmail.com>
2020-02-29 01:34:20 +0100

    Remove Admin and AdminPack directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const vendorUiPath = path.resolve(__dirname, 'vendor/sylius/ui-bundle');
 const build = require('./src/CorePack/recipe/config-builder');
 
-const backendConfig = build('backend', `./src/AdminPack/recipe/assets/backend/`, vendorUiPath);
+const backendConfig = build('backend', `./src/Monofony/Bundle/AdminBundle/Recipe/assets/backend/`, vendorUiPath);
 const frontendConfig = build('frontend', `./src/FrontPack/recipe/assets/frontend/`, vendorUiPath);
 
 module.exports = [backendConfig, frontendConfig];
