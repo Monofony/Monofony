5d5d23455baee59f4a0bf718c4d58e5f9e552434
Loic Fremont <lc.fremont@gmail.com>
2020-03-01 17:54:13 +0100

    Init Api Bundle

---
+++ src/Monofony/Bundle/ApiBundle/MonofonyApiBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Monofony package.
+ *
+ * (c) Monofony
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Monofony\Bundle\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MonofonyApiBundle extends Bundle
+{
+}
