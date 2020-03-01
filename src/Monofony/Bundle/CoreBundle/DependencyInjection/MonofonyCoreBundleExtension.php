05507ed0442fc342035e8af412904b60362d81f8
Loic Fremont <lc.fremont@gmail.com>
2020-03-01 14:31:11 +0100

    Tag doctrine event subscribers

--- src/Monofony/Bundle/CoreBundle/DependencyInjection/MonofonyCoreBundleExtension.php
+++ src/Monofony/Bundle/CoreBundle/DependencyInjection/MonofonyCoreBundleExtension.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace App\Monofony\Bundle\CoreBundle\DependencyInjection;
 
+use Doctrine\Common\EventSubscriber;
 use Sylius\Component\Customer\Context\CustomerContextInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -24,7 +25,19 @@ class MonofonyCoreBundleExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $this->tagCustomerContext($container);
+        $this->tagDoctrineEventSubscribers($container);
+    }
+
+    private function tagCustomerContext(ContainerBuilder $container): void
+    {
         $container->registerForAutoconfiguration(CustomerContextInterface::class)
             ->addTag('monofony.customer_context');
+    }
+
+    private function tagDoctrineEventSubscribers(ContainerBuilder $container): void
+    {
+        $container->registerForAutoconfiguration(EventSubscriber::class)
+            ->addTag('doctrine.event_subscriber');
     }
 }
