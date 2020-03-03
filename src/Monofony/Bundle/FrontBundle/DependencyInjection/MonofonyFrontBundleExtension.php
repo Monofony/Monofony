f278a3102cc441f29ab711acfdd4d37b7cc32aff
Loïc Frémont <lc.fremont@gmail.com>
2020-03-03 12:26:47 +0100

    Fix coding standard

--- src/Monofony/Bundle/FrontBundle/DependencyInjection/MonofonyFrontBundleExtension.php
+++ src/Monofony/Bundle/FrontBundle/DependencyInjection/MonofonyFrontBundleExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Monofony package.
+ *
+ * (c) Monofony
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Monofony\Bundle\FrontBundle\DependencyInjection;
 
 use Monofony\Bundle\FrontBundle\Menu\AccountMenuBuilderInterface;
