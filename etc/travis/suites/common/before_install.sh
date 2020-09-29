78f6e88d1b906493689ba2b9e62460d3215943d9
Loïc Frémont <lc.fremont@gmail.com>
2020-09-29 16:56:21 +0200

    Fix travis with main branch 0.x

--- etc/travis/suites/common/before_install.sh
+++ etc/travis/suites/common/before_install.sh
@@ -3,7 +3,7 @@
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 
 print_header "Customizing the environment" "AppName"
-run_command "git fetch origin master:refs/remotes/origin/master" || exit $? # Make origin/master available for is_suitable steps
+run_command "git fetch origin ${MAIN_BRANCH}:refs/remotes/origin/${MAIN_BRANCH}" || exit $? # Make origin/master available for is_suitable steps
 run_command "phpenv config-rm xdebug.ini" # Disable XDebug
 run_command "echo \"memory_limit=4096M\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $? # Increase memory limit to 4GB
 run_command "mkdir -p \"${APP_NAME_CACHE_DIR}\"" || exit $? # Create AppName cache directory
