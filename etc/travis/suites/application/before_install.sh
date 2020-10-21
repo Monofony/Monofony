f2504a9a281b06408bbfec1213f482686a58d358
Loic Fremont <lc.fremont@gmail.com>
2020-10-21 10:23:50 +0200

    Remove memcached on Travis

--- etc/travis/suites/application/before_install.sh
+++ etc/travis/suites/application/before_install.sh
@@ -3,9 +3,6 @@
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/application.sh"
 
-print_header "Activating memcached extension" "AppName"
-run_command "echo \"extension = memcached.so\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $?
-
 # Download and configure Symfony webserver
 print_header "Downloading Symfony CLI" "Sylius"
 if [ ! -f $APP_NAME_CACHE_DIR/symfony ]; then
