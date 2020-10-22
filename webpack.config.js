6bb46a27ad543f302b16e626dcc2c373a92be8f5
Loïc Frémont <lc.fremont@gmail.com>
2020-10-22 11:45:12 +0200

    Move core recipes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const vendorUiPath = path.resolve(__dirname, 'vendor/sylius/ui-bundle');
-const build = require('./src/Monofony/Bundle/CoreBundle/Recipe/config-builder');
+const build = require('./src/Monofony/Pack/CorePack/Recipe/config-builder');
 
 const backendConfig = build('backend', `./src/Monofony/Pack/AdminPack/Recipe/assets/backend/`, vendorUiPath);
 const frontendConfig = build('frontend', `./src/Monofony/Pack/FrontPack/Recipe/assets/frontend/`, vendorUiPath);
