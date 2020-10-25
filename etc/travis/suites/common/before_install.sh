e5f2030918e18391d156c51901a8e2544292f7c8
Loic Fremont <lc.fremont@gmail.com>
2020-10-25 18:28:54 +0100

    Fix package versions

--- etc/travis/suites/common/before_install.sh
+++ etc/travis/suites/common/before_install.sh
@@ -5,5 +5,5 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.s
 print_header "Customizing the environment" "Monofony"
 run_command "git fetch origin ${MAIN_BRANCH}:refs/remotes/origin/${MAIN_BRANCH}" || exit $? # Make origin/master available for is_suitable steps
 run_command "phpenv config-rm xdebug.ini" # Disable XDebug
-run_command "echo \"memory_limit=4096M\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $? # Increase memory limit to 4GB
+run_command "echo \"memory_limit=6144M\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $? # Increase memory limit to 6GB
 run_command "mkdir -p \"${APP_NAME_CACHE_DIR}\"" || exit $? # Create Monofony cache directory
