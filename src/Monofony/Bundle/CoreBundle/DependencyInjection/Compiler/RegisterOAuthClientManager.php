aa7efef4a675e8c0aaf7261d98ff065ba4ac32f7
Loïc Frémont <lc.fremont@gmail.com>
2020-10-25 15:03:37 +0100

    Fix client manager

--- src/Monofony/Bundle/CoreBundle/DependencyInjection/Compiler/RegisterOAuthClientManager.php
+++ src/Monofony/Bundle/CoreBundle/DependencyInjection/Compiler/RegisterOAuthClientManager.php
@@ -11,9 +11,12 @@
 
 namespace App\Monofony\Bundle\CoreBundle\DependencyInjection\Compiler;
 
+use App\Entity\OAuth\Client;
 use Monofony\Bridge\FOSOAuthServer\Entity\ClientManager;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\DependencyInjection\Reference;
 
 class RegisterOAuthClientManager implements CompilerPassInterface
 {
@@ -26,6 +29,11 @@ class RegisterOAuthClientManager implements CompilerPassInterface
             return;
         }
 
-        $container->register(ClientManager::class, '%fos_oauth_server.model.client.class%');
+        $definition = $container->register('monofony.client_manager', ClientManager::class);
+        $definition
+            ->setArguments([
+                new Reference('doctrine.orm.entity_manager'),
+                new Parameter('fos_oauth_server.model.client.class'),
+            ]);
     }
 }
