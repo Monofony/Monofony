0d49480f3950569d5a62d368394d290574fa4ce5
Loïc Frémont <lc.fremont@gmail.com>
2020-10-26 23:48:21 +0100

    Fix psalm tests

--- src/Monofony/Pack/ApiPack/.recipe/tests/Controller/JsonApiTestCase.php
+++ src/Monofony/Pack/ApiPack/.recipe/tests/Controller/JsonApiTestCase.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
-use Lakion\ApiTestCase\JsonApiTestCase as BaseJsonApiTestCase;
+use ApiTestCase\JsonApiTestCase as BaseJsonApiTestCase;
 
 class JsonApiTestCase extends BaseJsonApiTestCase
 {
