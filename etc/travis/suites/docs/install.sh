c9f77a7bdbfc54b19ed84da8b90942c78dd73cdb
Loïc Frémont <lc.fremont@gmail.com>
2021-01-05 20:42:27 +0100

    Bye bye Travis

--- etc/travis/suites/docs/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
-
-print_header "Installing" "Sphinx"
-run_command "pip install -q --user -r docs/requirements.txt"
