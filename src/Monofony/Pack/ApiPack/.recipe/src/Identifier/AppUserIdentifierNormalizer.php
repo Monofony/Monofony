025d2449a092409b5bcd5b4a0dfd790c4951fd07
Loïc Frémont <lc.fremont@gmail.com>
2021-01-13 17:42:20 +0100

    Me endpoints

---
+++ src/Monofony/Pack/ApiPack/.recipe/src/Identifier/AppUserIdentifierNormalizer.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Identifier;
+
+use Monofony\Contracts\Core\Identifier\AppUserIdentifierNormalizerInterface;
+use Monofony\Contracts\Core\Model\User\AppUserInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Core\Security;
+
+final class AppUserIdentifierNormalizer implements AppUserIdentifierNormalizerInterface
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @psalm-suppress InvalidReturnType
+     * @psalm-suppress InvalidReturnStatement
+     */
+    public function denormalize($data, $type, $format = null, array $context = [])
+    {
+        $user = $this->security->getUser();
+
+        if (null === $user || !$user instanceof AppUserInterface) {
+            throw new AccessDeniedHttpException();
+        }
+
+        return (string) $user->getCustomer()->getId();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return 'me' === $data;
+    }
+}
