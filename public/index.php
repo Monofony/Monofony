1391a70858eeab573b3cab9e2794f767cdbd3448
Loïc Frémont <lc.fremont@gmail.com>
2021-06-14 10:20:07 +0200

    Fix bootstrap

--- public/index.php
+++ public/index.php
@@ -7,7 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
