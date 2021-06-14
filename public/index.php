99f5479ae4e5a6218491b105894a9a42ae039eb4
Loïc Frémont <lc.fremont@gmail.com>
2021-06-14 11:39:15 +0200

    Fix form help

--- public/index.php
+++ public/index.php
@@ -5,10 +5,12 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
-
 if (method_exists(Dotenv::class, 'bootEnv')) {
+    require dirname(__DIR__).'/vendor/autoload.php';
+
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+} else {
+    require dirname(__DIR__).'/config/bootstrap.php';
 }
 
 if ($_SERVER['APP_DEBUG']) {
