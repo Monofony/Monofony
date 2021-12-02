b0eb8aa1bc2d2bbc82a90cf34e41f24c15eef3f5
Loic Fremont <lc.fremont@gmail.com>
2021-12-02 19:36:54 +0100

    Fix coding standard

--- src/Monofony/Pack/CorePack/.recipe/src/Fixture/Factory/AppUserExampleFactory.php
+++ src/Monofony/Pack/CorePack/.recipe/src/Fixture/Factory/AppUserExampleFactory.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Fixture\Factory;
 
-use Faker\Generator;
 use Faker\Factory;
+use Faker\Generator;
 use Monofony\Contracts\Core\Model\Customer\CustomerInterface;
 use Monofony\Contracts\Core\Model\User\AppUserInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
