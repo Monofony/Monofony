bc3aad98f7e2985aeeedc99b3381fda5e96b2ead
Loïc Frémont <lc.fremont@gmail.com>
2021-12-03 12:04:44 +0100

    Fix coding standard

--- src/Monofony/Pack/CorePack/.recipe/src/Entity/Customer/Customer.php
+++ src/Monofony/Pack/CorePack/.recipe/src/Entity/Customer/Customer.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Entity\Customer;
 
-use Webmozart\Assert\Assert;
 use Doctrine\ORM\Mapping as ORM;
 use Monofony\Contracts\Core\Model\Customer\CustomerInterface;
 use Monofony\Contracts\Core\Model\User\AppUserInterface;
 use Sylius\Component\Customer\Model\Customer as BaseCustomer;
 use Sylius\Component\User\Model\UserInterface;
 use Symfony\Component\Validator\Constraints\Valid;
+use Webmozart\Assert\Assert;
 
 /**
  * @ORM\Entity
