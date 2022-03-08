5083562a2bfbb1c6ec295c75490c6472118ff00e
Loïc Frémont <lc.fremont@gmail.com>
2022-03-08 16:28:18 +0100

    Remove Sylius forks

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const vendorUiPath = path.resolve(__dirname, 'vendor/monofony/ui-bundle');
+const vendorUiPath = path.resolve(__dirname, 'vendor/sylius/ui-bundle');
 const build = require('./src/Monofony/MetaPack/CoreMeta/.recipe/config-builder');
 
 const backendConfig = build('backend', `./src/Monofony/MetaPack/AdminMeta/.recipe/assets/backend/`, vendorUiPath);
