b125c1742209cd962e1f2717cc9bc6f7f10dc603
Loïc Frémont <lc.fremont@gmail.com>
2022-04-24 02:24:34 +0200

    Bump Phpstan Symfony package

--- src/Monofony/MetaPack/FrontMeta/.recipe/tests/Behat/Context/Ui/Frontend/HomepageContext.php
+++ src/Monofony/MetaPack/FrontMeta/.recipe/tests/Behat/Context/Ui/Frontend/HomepageContext.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Behat\Context\Ui\Frontend;
 
-use App\Tests\Behat\Page\Frontend\HomePage;
 use Behat\Behat\Context\Context;
 
 class HomepageContext implements Context
 {
-    public function __construct(private HomePage $homePage)
-    {
-    }
 }
