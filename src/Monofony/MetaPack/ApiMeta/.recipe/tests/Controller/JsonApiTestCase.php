1e688a39c55297a5cc5365f55f148f370651aa00
Loïc Frémont <lc.fremont@gmail.com>
2022-04-26 10:37:51 +0200

    Use factories on api tests

--- src/Monofony/MetaPack/ApiMeta/.recipe/tests/Controller/JsonApiTestCase.php
+++ src/Monofony/MetaPack/ApiMeta/.recipe/tests/Controller/JsonApiTestCase.php
@@ -12,8 +12,7 @@ class JsonApiTestCase extends BaseJsonApiTestCase
     {
         parent::__construct($name, $data, $dataName);
 
-        $this->dataFixturesPath = __DIR__.'/../DataFixtures/ORM';
-        $this->expectedResponsesPath = __DIR__.'/../Responses/Expected';
+        $this->expectedResponsesPath = __DIR__.'/../Responses';
     }
 
     /**
