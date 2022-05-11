f547415201a3f5f93629cb6e6c46ff6f69fa0133
Loïc Frémont <lc.fremont@gmail.com>
2022-05-11 16:52:41 +0200

    Preview uploaded files

--- src/Monofony/MetaPack/AdminMeta/.recipe/assets/backend/js/app-images-preview.js
+++ src/Monofony/MetaPack/AdminMeta/.recipe/assets/backend/js/app-images-preview.js
@@ -1,29 +1,29 @@
 import $ from 'jquery';
 
 const displayUploadedImage = function displayUploadedImage(input) {
-  if (input.files && input.files[0]) {
-    const reader = new FileReader();
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
 
-    reader.onload = (event) => {
-      const image = $(input).parent().siblings('.image');
+        reader.onload = (event) => {
+            const image = $(input).parent().siblings('.image');
 
-      if (image.length > 0) {
-        image.attr('src', event.target.result);
-      } else {
-        const img = $('<img class="ui small bordered image"/>');
-        img.attr('src', event.target.result);
-        $(input).parent().before(img);
-      }
-    };
+            if (image.length > 0) {
+                image.attr('src', event.target.result);
+            } else {
+                const img = $('<img class="ui small bordered image"/>');
+                img.attr('src', event.target.result);
+                $(input).parent().before(img);
+            }
+        };
 
-    reader.readAsDataURL(input.files[0]);
-  }
+        reader.readAsDataURL(input.files[0]);
+    }
 };
 
 $.fn.extend({
-  previewUploadedImage(root) {
-    $(root).on('change', 'input[type="file"]', function() {
-      displayUploadedImage(this);
-    });
-  },
+    previewUploadedImage(root) {
+        $(root).on('change', 'input[type="file"]', function () {
+            displayUploadedImage(this);
+        });
+    },
 });
