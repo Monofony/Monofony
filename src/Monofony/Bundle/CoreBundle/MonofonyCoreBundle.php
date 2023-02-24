d4724378c185d1c7f2e3015241ac6fc22e5fa1e8
Loïc Frémont <lc.fremont@gmail.com>
2023-02-24 08:59:19 +0100

    Set version as 0.10.0-dev

--- src/Monofony/Bundle/CoreBundle/MonofonyCoreBundle.php
+++ src/Monofony/Bundle/CoreBundle/MonofonyCoreBundle.php
@@ -24,7 +24,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MonofonyCoreBundle extends Bundle
 {
-    public const VERSION = '0.9.0';
+    public const VERSION = '0.10.0-dev';
 
     /**
      * {@inheritdoc}
